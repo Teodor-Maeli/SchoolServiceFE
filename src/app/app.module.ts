ca93f5df659b2a50a9bb8e4fbdfe555bbceaed6e
teodor.maeli <teodor.maeli@brainstarstech.com>
2022-07-31 15:01:46 +0300

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StudentsComponent } from './students/students.component';
+import { TeachersComponent } from './teachers/teachers.component';
+import { EnrollmentsComponent } from './enrollments/enrollments.component';
+import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentsComponent,
+    TeachersComponent,
+    EnrollmentsComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule
